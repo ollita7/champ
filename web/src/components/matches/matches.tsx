067e661f035c984ac12b125ed55cf6ac601807c2
Guillermo Fernandez <ollita7@mail.com>
2023-01-27 16:17:28 -0300

    add expand

--- web/src/components/matches/matches.tsx
+++ web/src/components/matches/matches.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement, useState } from 'react';
 import './styles.scss'
 import Typography from '@mui/material/Typography';
-import ReactCountryFlag from "react-country-flag"
 import { CircleFlag } from 'react-circle-flags'
 import Paper from '@mui/material/Paper';
 import data from '../../db/matches.json';
@@ -20,42 +19,55 @@ export interface IGroupsProps {
 }
 
 const Groups: React.FC<IGroupsProps> = ({ group, ...props }): ReactElement => {
-  const distantFuture = new Date(8640000000000000)
-  const matches = data.matches
-                          .filter(match => match.group == group)
-                          .sort((a,b) => {
-                            let dateA = a.date !== 'TBD' ? new Date(a.date) : distantFuture;
-                            let dateB = b.date !== 'TBD' ? new Date(b.date) : distantFuture;
-                            return dateA.getTime() - dateB.getTime()
-                          });
+  const [expanded, setExpanded] = useState<boolean>(false);
+
+  const groupMatches = data.matches
+                          .filter(match => match.group == group );
+  const defined_matches = groupMatches.filter(match => match.date != 'TBD')
+                                      .sort((a,b) => {
+                                        let dateA = new Date(a.date);
+                                        let dateB = new Date(b.date);
+                                        return dateA.getTime() - dateB.getTime()
+                                      });
+
+  const tbd_matches = groupMatches.filter(match => match.date == 'TBD');
+     
+  const renderMatch = (match) => {
+    return (<div className='match' key={match.id}>
+        <Item elevation={4}>
+          <div className='details'>
+            <span className='date'><Typography>
+              {match.date === 'TBD' ?
+                <strong>{match.date}</strong>
+                :new Date(match.date).toLocaleDateString('es-es', { weekday:"long", year:"numeric", month:"short", day:"numeric", hour: "2-digit", minute: "2-digit"}) 
+              }</Typography></span>
+              {<span className='title'><Typography fontSize={11}>{group}</Typography></span>}
+          </div>
+          <div className='player'>
+            <div className='img'>
+              <CircleFlag countryCode="uy" height="25"/>
+            </div>
+            <span className='member'><Typography>{match.player_1.name}</Typography></span>
+          </div>
+          <div className='player'>
+            <div className='img'>
+              <CircleFlag countryCode="uy" height="25"/>
+            </div>
+            <span className='member'><Typography >{match.player_2.name}</Typography></span>
+          </div>
+        </Item>
+      </div>);
+  }
+
   return (
     <div className='matches'>
-      {matches.map(match => 
-        <div className='match' key={match.id}>
-          <Item elevation={4}>
-            <div className='details'>
-              <span className='date'><Typography>
-                {match.date === 'TBD' ?
-                  <strong>{match.date}</strong>
-                  :new Date(match.date).toLocaleDateString('es-es', { weekday:"long", year:"numeric", month:"short", day:"numeric", hour: "2-digit", minute: "2-digit"}) 
-                }</Typography></span>
-                {<span className='title'><Typography fontSize={11}>{group}</Typography></span>}
-            </div>
-            <div className='player'>
-              <div className='img'>
-                <CircleFlag countryCode="uy" height="25"/>
-              </div>
-              <span className='member'><Typography>{match.player_1.name}</Typography></span>
-            </div>
-            <div className='player'>
-              <div className='img'>
-                <CircleFlag countryCode="uy" height="25"/>
-              </div>
-              <span className='member'><Typography >{match.player_2.name}</Typography></span>
-            </div>
-          </Item>
+      {defined_matches.map(match => renderMatch(match))}
+      <Typography><div className='more-less' onClick={() => setExpanded(!expanded)}>{expanded ? 'Ver (-)' : 'Ver (+)'}</div></Typography>
+      {expanded &&
+        <div className='expand'>
+          {tbd_matches.map(match => renderMatch(match))}
         </div>
-      )}
+      }
     </div>
   )
 }
