c0bfad1d6cbd37bf7033033bd8b6654791a27e46
Guillermo Fernandez <ollita7@mail.com>
2023-02-01 18:20:29 -0300

    add small change

--- web/src/screens/terms/terms.tsx
+++ web/src/screens/terms/terms.tsx
@@ -1,24 +1,15 @@
 import React, { ReactElement, useState } from 'react';
-import { Document, Page } from 'react-pdf';
 import ResponsiveMenu from '../../components/menu/menu';
 import './styles.scss'
 
 const Terms: React.FC = ({ ...props }): ReactElement => {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
   return (
     <div className='terms'>
       <ResponsiveMenu/>
-      <Document file="reglamento.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
+      <div className='pdf'>
+        <iframe className='iframe' src={'https://ocean-tour.netlify.app/reglamento.pdf'} />
+      </div>
+      
     </div>
   );
 }
