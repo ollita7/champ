e7f3ecdbcb261b1086b313ba591a9448b91f63af
Guillermo Fernandez <ollita7@mail.com>
2023-02-01 11:20:31 -0300

    add small changes

--- web/static/index.tsx
+++ web/static/index.tsx
@@ -2,5 +2,4 @@ import ReactDOM from 'react-dom';
 import { App } from '../src/app';
 import './styles.scss';
 import './modals.scss';
-import './forms.scss';
 ReactDOM.render(<App />, document.getElementById('root'));
