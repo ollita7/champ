d7ec4b96d04994d14a24bb69435453c9e6eeeef6
Guillermo Fernandez <ollita7@mail.com>
2023-02-07 16:26:33 -0300

    add login

--- api/src/models/user.model.ts
+++ api/src/models/user.model.ts
@@ -1,7 +1,12 @@
 import { IsString } from "kiwi-server";
 
 export class UserIn{
-    @IsString() name: string;
-    @IsString() email: string;
-    @IsString() picture: string;
-  }
+  @IsString() name: string;
+  @IsString() email: string;
+  @IsString() picture: string;
+}
+
+export class LoginIn{
+  @IsString() username: string;
+  @IsString() password: string;
+}
